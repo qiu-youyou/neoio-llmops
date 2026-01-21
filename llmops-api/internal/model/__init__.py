bd13766b9435681dc0aece1c9d81c3cd935e9658
youyou <s.qiu@foxmail.com>
2026-01-21 22:16:50 +0800

    feat: 初始化会话服务

--- llmops-api/internal/model/__init__.py
+++ llmops-api/internal/model/__init__.py
@@ -7,6 +7,7 @@
 """
 from .api_tool import ApiTool, ApiToolProvider
 from .app import App, AppDatasetJoin
+from .conversation import Conversation, Message, MessageAgentThought
 from .dataset import Dataset, Document, Segment, KeywordTable, DatasetQuery, ProcessRule
 from .upload_file import UploadFile
 
@@ -14,5 +15,6 @@ __all__ = [
     "App", "AppDatasetJoin",
     "ApiTool", "ApiToolProvider",
     "Dataset", "Document", "Segment", "KeywordTable", "DatasetQuery", "ProcessRule",
-    "UploadFile"
+    "UploadFile",
+    "Conversation", "Message", "MessageAgentThought",
 ]
